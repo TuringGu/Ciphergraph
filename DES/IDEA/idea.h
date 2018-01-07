3f303384d7cf8cb63a07a2a45cc4a1668193a832
Idea <gulintao850366208@gmail.com>
2018-01-07 19:25:23 +0800

    des

---
+++ DES/IDEA/idea.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifdef _IDEA_DOT_H
+#define _IDEA_DOT_H
+
+#include <stdio.h>
+#include <time.h>
+#include <process.h>
+#include <io.h>
+#include <string.h>
+#include <conio.h>
+#include <iostream>
+
+#define IDEAKEYSIZE 16
+#define IDEABLOCKSIZE 8
+#define word16 unsigned short int
+
+
+#endif // _IDEA_DOT_H
